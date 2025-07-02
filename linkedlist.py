1a3ac958371ec7b09d56b79a462e3854088f27ff
Karthick-Roshan <karthickroshan618@gmail.com>
2025-07-02 19:49:26 +0530

    'linkedlist'

--- linkedlist.py
+++ linkedlist.py
@@ -18,3 +18,24 @@ def reverseList(head):
         
     curr.next = None
     return head
+
+# Given the head of a linked list and the number k, Your task is to find the kth node from the end. 
+# If k is more than the number of nodes, then the output should be -1.
+
+def getKthFromLast(self, head, k):
+    curr = head
+    temp = head
+    
+    count = 0
+    while curr and count < k:
+        curr = curr.next
+        count += 1
+        
+    if count < k:
+        return -1
+        
+    while curr:
+        temp = temp.next
+        curr = curr.next
+        
+    return temp.data if temp else -1
