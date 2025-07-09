d55a8b55fa2f0c37e29c07cf2814d67292bebe92
Karthick-Roshan <karthickroshan618@gmail.com>
2025-07-09 15:42:16 +0530

    'string'

--- strings.py
+++ strings.py
@@ -37,4 +37,42 @@ def isIsomorphic(s: str, t: str) -> bool:
 
     return True
 
-print(isIsomorphic("egg", "add"))
+# print(isIsomorphic("egg", "add"))
+
+
+# Implement Atoi
+# Given a string s, the objective is to convert it into integer format without utilizing any built-in functions. 
+
+# Cases for atoi() conversion:
+
+# Skip any leading whitespaces.
+# Check for a sign (‘+’ or ‘-‘), default to positive if no sign is present.
+# Read the integer by ignoring leading zeros until a non-digit character is encountered or end of the string is reached. If no digits are present, return 0.
+# If the integer is greater than 231 – 1, then return 231 – 1 and if the integer is smaller than -231, then return -231.
+
+def myAtoi(s: str) -> int:
+    n = len(s)
+    sign = 1
+    idx = 0
+    res = 0
+    
+    while (idx < n) and s[idx] == " ":
+        idx += 1
+        
+    if idx < n and (s[idx] == "-" or s[idx] == "+"):
+        if s[idx] == "-":
+            sign = -1
+        idx += 1
+    
+    while idx < n and '0' <= s[idx] <= '9':
+        
+        res = 10 * res + (ord(s[idx]) - ord('0')) 
+        
+        if res > (2 ** 31 - 1):
+            return sign * (2 ** 31 - 1) if sign == 1 else -2 ** 31
+            
+        idx += 1
+        
+    return sign * res
+
+print(myAtoi("42"))
