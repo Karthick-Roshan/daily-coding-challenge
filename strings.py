25bffa99c776f39e8b50c3b865d4e502eeaca8c5
Karthick-Roshan <karthickroshan618@gmail.com>
2025-07-14 22:02:52 +0530

    'string'

--- strings.py
+++ strings.py
@@ -119,3 +119,22 @@ def findTheDifference(s, t):
             return chr(c + ord('a'))
 
 # print(findTheDifference("abcd", "abcde"))
+
+
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+def lengthOfLongestSubstring(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+print(lengthOfLongestSubstring("abcabcbb"))
