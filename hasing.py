61dbfbaa58bc11b5d6280f4a3e20adce20f21b5b
Karthick-Roshan <karthickroshan618@gmail.com>
2025-08-02 22:48:34 +0530

    'hash'

--- hasing.py
+++ hasing.py
@@ -12,4 +12,23 @@ def countPairs(arr, target):
             
     return count
 
-print(countPairs([1, 5, 7, -1, 5], 6)) 
+# print(countPairs([1, 5, 7, -1, 5], 6)) 
+
+
+# First unique character in a given string
+def first_unique_char(s):
+    char_count = {}
+    
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    for i, char in enumerate(s):
+        if char_count[char] == 1:
+            return i
+
+    return -1
+
+# print(first_unique_char("leetcode"))  
