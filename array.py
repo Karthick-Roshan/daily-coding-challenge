7aa65f2280d8566e0de7f16c6c3e92dff09fc9de
Karthick-Roshan <karthickroshan618@gmail.com>
2025-08-18 23:37:01 +0530

    'array'

--- array.py
+++ array.py
@@ -866,3 +866,28 @@ def new21Game(n: int, k: int, maxPts: int) -> float:
     return result
 
 # print(new21Game(n = 10, k = 1, maxPts = 10))
+
+import heapq
+def findRelativeRanks(score: list[int]) -> list[str]:
+    n = len(score)
+    result = [""] * n
+    
+    heap = [(-s, i) for i, s in enumerate(score)]
+    heapq.heapify(heap)
+    
+    rank = 1
+    while heap:
+        _, idx = heapq.heappop(heap)
+        if rank == 1:
+            result[idx] = "Gold Medal"
+        elif rank == 2:
+            result[idx] = "Silver Medal"
+        elif rank == 3:
+            result[idx] = "Bronze Medal"
+        else:
+            result[idx] = str(rank)
+        rank += 1
+    
+    return result
+
+# print(findRelativeRanks([10,3,8,9,4]))
