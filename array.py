6bb6063b9dc9e45a900c85e56ed88f0794392cd7
Karthick-Roshan <karthickroshan618@gmail.com>
2025-08-28 19:53:48 +0530

    'array'

--- array.py
+++ array.py
@@ -963,3 +963,22 @@ def kLargestElements(arr, k):
     return res
 
 # print(kLargestElements([1, 23, 12, 9, 30, 2, 50], 3))
+
+# Get Minimum Difference
+def getMinDiff(arr: list[int], k: int) -> int:
+    n = len(arr)
+    arr.sort()
+    
+    res = arr[n - 1] - arr[0]
+    
+    for i in range(1, n):
+        if arr[i] - k < 0:
+            continue
+        
+        low = min(arr[0] + k, arr[i] - k)
+        upp = max(arr[i - 1] + k, arr[n - 1] - k)
+        res = min(res, upp - low)
+        
+    return res
+
+print(getMinDiff([1, 5, 8, 10], 2))
