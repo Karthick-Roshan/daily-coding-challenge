4929eddf9ebd86dce513086a25a285eae3431ab2
Karthick-Roshan <karthickroshan618@gmail.com>
2025-09-08 23:29:37 +0530

    'math'

--- array.py
+++ array.py
@@ -1069,3 +1069,14 @@ def addBinary(a: str, b: str) -> str:
     return bin(int(a, 2) + int(b, 2))[2:]
 
 # print(addBinary(a = "11", b = "1"))
+
+def getNoZeroIntegers(n: int) -> list[int]:
+    def noZero(n: int) -> bool:
+        return '0' not in str(n)
+
+    for i in range(1, n):
+        b = n - i
+        if noZero(i) and noZero(b):
+            return [i, b]
+
+print(getNoZeroIntegers(11))
